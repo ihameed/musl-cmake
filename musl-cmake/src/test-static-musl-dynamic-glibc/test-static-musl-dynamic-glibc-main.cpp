e4d07d83cedf6516e38a85b4f385db1314e1ae4b
Imran Hameed <idhameed@gmail.com>
2022-09-06 11:27:48 -0700

    Checkpoint

--- musl-cmake/src/test-static-musl-dynamic-glibc/test-static-musl-dynamic-glibc-main.cpp
+++ musl-cmake/src/test-static-musl-dynamic-glibc/test-static-musl-dynamic-glibc-main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <dlfcn.h>
+#include <stdint.h>
 
 void *
 thread_proc(void *) {
@@ -12,9 +13,31 @@ thread_proc(void *) {
     return nullptr;
 }
 
+struct dynamic_loader_global_data_t {
+        int debug_mask;
+        char const *platform;
+        size_t platform_len;
+        size_t page_size;
+        size_t min_sig_stack_size;
+        int inhibit_cache;
+        int clock_tick;
+        int verbose;
+        int debug_fd;
+        int lazy;
+        int bind_not;
+        int dynamic_weak;
+        int fpu_control;
+        uint64_t hwcap;
+        uint64_t hwcap_mask;
+        uintptr_t *auxv;
+};
+
 extern "C"
 int
 main(int, char **) {
+    auto const ptr = static_cast<dynamic_loader_global_data_t const *>(dlsym(RTLD_DEFAULT, "_rtld_global_ro"));
+    printf("got ptr = %p\n", ptr);
+    printf("got auxv = %p\n", ptr->auxv);
     auto const dll = dlopen("/usr/lib/libc.so.6", RTLD_LOCAL);
     auto const raw_glibc_printf = dlsym(dll, "printf");
     auto const glibc_printf = reinterpret_cast<int (*) (char const *, ...)>(raw_glibc_printf);
